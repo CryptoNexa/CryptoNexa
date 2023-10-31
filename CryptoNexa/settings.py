b9ad6fce09103d34875922178f87790cfb0420ff
jhanviVadher29 <jhanvivadher10@gmail.com>
2023-10-31 13:17:25 -0400

    Implemented models for crypto app

--- CryptoNexa/settings.py
+++ CryptoNexa/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'CryptoNexa'
+    'CryptoNexa',
+    'crypto',
 ]
 
 MIDDLEWARE = [
--- crypto/models.py
+++ crypto/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Crypto(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField(max_length=10)
+    oneHourPrice = models.FloatField()
+    oneHourFlag = models.FloatField()
+    TwentyFourHour = models.FloatField()
+    TwentyFourPrice = models.FloatField()
+    MarketCap = models.IntegerField()
+    Volume = models.IntegerField()
+
+    def __str__(self):
+        return self.name
