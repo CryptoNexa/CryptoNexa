5164a9dde676791766ff5d97e1d89d00c65f9282
Shaurya Sharma <>
2023-11-04 20:03:57 -0400

    adding form for currency exchange and updating currency model

--- currencyExchange/models.py
+++ currencyExchange/models.py
@@ -5,8 +5,6 @@ from django.db import models
 class Currency(models.Model):
     code = models.CharField(max_length=3)
     name = models.CharField(max_length=50)
-    logo = models.CharField(max_length=100)
-    symbol = models.CharField(max_length=2)
 
     def __str__(self):
         return self.name
