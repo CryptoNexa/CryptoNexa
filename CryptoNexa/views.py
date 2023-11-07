0a0a2f8d0e5610ff2fbac287ea1264aa5cd9df77
Chintal Raval <chintaldraval@gmail.com>
2023-11-06 20:36:29 -0500

    WIP | Crypto Detail View

--- CryptoNexa/views.py
+++ CryptoNexa/views.py
@@ -1,18 +1,21 @@
 # Create your views here.
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
 from core.apis.coinmarketcap.fetch_data import fetch_data
-from core.apis.coinmarketcap.test import data
+from core.apis.coinmarketcap.test import data_usd, data_cad
 from core.models import Cryptocurrency, Quote
 
 
 def index(request):
     error = ''
-    fetched_data_from_api = fetch_data()
-    # fetched_data_from_api = data
-    fetched_status = fetched_data_from_api.get('status')
+    # fetched_data_from_api_USD = fetch_data('USD')
+    fetched_data_from_api_USD = data_usd
+    fetched_data_from_api_CAD = data_cad
+    # fetched_data_from_api_CAD = fetch_data('CAD')
+    fetched_status = fetched_data_from_api_USD.get('status')
     if fetched_status.get('error_code') != 0:
         error = 'An error occurred while fetching the data.'
         context = {
@@ -20,23 +23,35 @@ def index(request):
         }
         return render(request, 'crypto/crypto_list_view.html', context=context)
 
-    crypto_data = fetched_data_from_api.get('data')
-
+    crypto_data = fetched_data_from_api_USD.get('data')
+    crypto_data_cad = fetched_data_from_api_CAD.get('data')
     for current_crypto in crypto_data:
-        quote_obj = Quote.objects.create(currency_slug=current_crypto.get('slug'),
-                                         currency_symbol=current_crypto.get('symbol'),
-                                         data=current_crypto.get('quote'))
+        quote_obj, created = Quote.objects.get_or_create(currency_slug=current_crypto.get('slug'),
 
-        Cryptocurrency.objects.create(name=current_crypto.get('name'), symbol=current_crypto.get('symbol'),
-                                      slug=current_crypto.get('slug'),
-                                      num_market_pairs=current_crypto.get('num_market_pairs'),
-                                      circulating_supply=current_crypto.get('circulating_supply'),
-                                      total_supply=current_crypto.get('total_supply'),
-                                      max_supply=current_crypto.get('max_supply'),
-                                      infinite_supply=current_crypto.get('infinite_supply'),
-                                      date_added=current_crypto.get('date_added'),
-                                      last_updated=current_crypto.get('last_updated'),
-                                      quote=quote_obj)
+                                      currency_symbol=current_crypto.get('symbol'),
+                                      data=current_crypto.get('quote'))
+        quote_obj.data = current_crypto.get('quote')
+        quote_obj.save()
+
+        Cryptocurrency.objects.get_or_create(name=current_crypto.get('name'), symbol=current_crypto.get('symbol'),
+                                             slug=current_crypto.get('slug'),
+                                             num_market_pairs=current_crypto.get('num_market_pairs'),
+                                             circulating_supply=current_crypto.get('circulating_supply'),
+                                             total_supply=current_crypto.get('total_supply'),
+                                             max_supply=current_crypto.get('max_supply'),
+                                             infinite_supply=current_crypto.get('infinite_supply'),
+                                             date_added=current_crypto.get('date_added'),
+                                             last_updated=current_crypto.get('last_updated'),
+                                             quote=quote_obj)
+
+    for crypto_cad in crypto_data_cad:
+        try:
+            quote_obj = Quote.objects.get(currency_slug=crypto_cad.get('slug'))
+            quote = quote_obj.data
+            quote['CAD'] = crypto_cad.get('quote').get('CAD')
+            quote_obj.save()
+        except:
+            pass
 
     cryptos = Cryptocurrency.objects.all()
     context = {
@@ -44,4 +59,3 @@ def index(request):
     }
 
     return render(request, 'CryptoNexa/index.html', context=context)
-
--- core/apis/coinmarketcap/fetch_data.py
+++ core/apis/coinmarketcap/fetch_data.py
@@ -3,25 +3,24 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
 
-# url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-parameters = {
-    'start': '1',
-    'limit': '100',
-    'convert': 'USD',
-    'cryptocurrency_type': 'all',
-    'tag': 'all',
-    'sort': 'market_cap'
-}
+
 headers = {
     'Accepts': 'application/json',
     'X-CMC_PRO_API_KEY': 'dd538bfb-5c46-4f03-836d-08a33c7c5e51',
 }
 
 
-def fetch_data():
+def fetch_data(currency):
     session = Session()
     session.headers.update(headers)
+
+    parameters = {
+        'start': '1',
+        'limit': '100',
+        'convert': currency,
+        'cryptocurrency_type': 'all',
+    }
 
     try:
         print("Headers: ", headers)
