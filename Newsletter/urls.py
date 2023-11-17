7bc747b1a60f229cc3ecf372cdc08e0a83e6f5e1
jhanviVadher29 <jhanvivadher10@gmail.com>
2023-11-17 17:14:00 -0500

    newsletter done

--- Newsletter/urls.py
+++ Newsletter/urls.py
@@ -3,6 +3,6 @@ from .views import news_list, subscribe, success_view
 
 urlpatterns = [
     path('news/', news_list, name='news_list'),
-    path('subscribe/', subscribe, name='subscribe'),
+    path('news/subscribe/', subscribe, name='subscribe'),
     path('success/', success_view, name='success')
 ]
