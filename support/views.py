c38e849405574f9216e2e144e3e5155a353360d8
kinjal <kinjaldoshi1398@gmail.com>
2023-11-20 18:10:39 -0500

    Template and view modified for issue creation when user logged in and not logged in

--- support/views.py
+++ support/views.py
@@ -1,4 +1,5 @@
 # your_app/views.py
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from core.models import User
 from .forms import SupportForm
@@ -6,21 +7,33 @@ from .models import SupportIssue
 
 
 def issue_creation(request):
-    user_id = request.user.id
-    user = User.objects.get(id=user_id)
+    issue_types = [
+        ('Currency Exchange', 'fa-exchange-alt'),
+        ('Buy/Sell Orders', 'fa-money-check-alt'),
+        ('Portfolio', 'fa-chart-pie'),
+        ('Purchase', 'fa-credit-card'),
+        ('Unauthorized Charge', 'fa-exclamation-triangle'),
+        ('Refunds', 'fa-undo'),
+    ]
 
     if request.method == 'POST':
+        user_id = request.user.id
+        user = User.objects.get(id=user_id)
         form = SupportForm(request.POST, request.FILES, user=user)
-        file = request.FILES['file']
         if form.is_valid():
             support_issue = form.save(commit=False)
             support_issue.user = user
             support_issue.save()
             return redirect('support:list_issues')  # Redirect to a success page
     else:
-        form = SupportForm(user=user)
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            user = User.objects.get(id=user_id)
+            form = SupportForm(user=user)
+        else:
+            return render(request, 'support/issue_creation.html', {'issue_types': issue_types})
 
-    return render(request, 'support/issue_creation.html', {'form': form})
+    return render(request, 'support/issue_creation.html', {'form': form, 'issue_types': issue_types})
 
 
 def list_issues(request):
