79380cc3d9eb0e80384b07ffa8b0aab42c4fc2e3
pharshil1902 <pharshil1902@gmail.com>
2023-11-20 19:53:20 -0500

    Added Sell Transaction Flow

--- CryptoNexa/urls.py
+++ CryptoNexa/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from CryptoNexa import views
+from CryptoNexa import views, settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +28,4 @@ urlpatterns = [
     path('currencyExchange/', include('currencyExchange.urls')),
     path('', include('payments.urls')),
     path('', include('BuySell.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
