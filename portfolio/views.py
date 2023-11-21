1d7546012fda01097a91753a9dbf41b8cfff2ca6
jhanviVadher29 <jhanvivadher10@gmail.com>
2023-11-21 17:02:34 -0500

    Portfolio done

--- portfolio/views.py
+++ portfolio/views.py
@@ -10,9 +10,24 @@ def portfolio(request):
     user_id = request.user.id
     user_transactions = Transaction.objects.filter(user=current_user)
     user_object = User.objects.get(pk=user_id)
+    total = 0
+    Total1 = 0
+    total_sell = 0
 
-    total = sum(transaction.quantity * transaction.price for transaction in user_transactions)
+    for transaction in user_transactions:
+        if transaction.type == 'buy':
+
+            total += transaction.quantity * transaction.price
+            Total1 = total
+            print(Total1)
+        elif transaction.type == 'sell':
+
+            total_sell += transaction.quantity * transaction.price
+            total -= transaction.quantity * transaction.price
+            Total2 = total
     context = {
-        "user_object": user_object, "transaction_obj": user_transactions, "total": total
+        "user_object": user_object, "transaction_obj": user_transactions, "total": total, "buy": Total1,
+        "sell": total_sell
     }
+
     return render(request, 'portfolio/portfolio.html', context)
