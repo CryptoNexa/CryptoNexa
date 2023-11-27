ed53b98e7454085ea2dd49f7b6c147d80eabc7ec
pharshil1902 <pharshil1902@gmail.com>
2023-11-26 22:05:52 -0500

    minor change

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from core.models import User
 from django.dispatch import receiver
@@ -16,8 +17,8 @@ class UserPaymentSell(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     payment_status = models.BooleanField(default=False)
     transaction_id = models.ForeignKey(Transaction, on_delete=models.CASCADE, null=True)
-    account_holder_name = models.CharField(max_length=255, blank=True, null=True)
-    account_number = models.CharField(max_length=30, blank=True, null=True)
-    transit_number = models.CharField(max_length=9, blank=True, null=True)  # Canada
-    routing_number = models.CharField(max_length=9, blank=True, null=True)  # USA
-    amount = models.DecimalField(max_digits=10, decimal_places=4, blank=True, null=True)
+    account_holder_name = models.CharField(max_length=255)
+    account_number = models.CharField(max_length=30)
+    transit_number = models.CharField(max_length=9)  # Canada
+    routing_number = models.CharField(max_length=9)  # USA
+    amount = models.DecimalField(max_digits=10, decimal_places=4)
